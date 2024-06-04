8d523d6c1c968a5d986254c134a2d631ae51ff93
romeoxaryan <168858644+romeoxaryan@users.noreply.github.com>
2024-06-04 19:18:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createBot () {
 const bot = mineflayer.createBot({
   host: 'RomeoBot.aternos.me', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'NooB', 
+  username: 'R0M30', 
   port: 34082, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
